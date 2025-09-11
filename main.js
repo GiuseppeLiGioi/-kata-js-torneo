a5327dd886d33831be8679223c795f366087308d
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-09-11 14:48:40 +0200

    added. bonus 2. start and reset buttons

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 
-let copyFighters = fighters.map(f => ({name: f.name, power: f.power}))
-let copyWeapons = weapons.map(w => ({name: w.name, power: w.power}))
+let copyFighters = [];
+let copyWeapons = [];
+let qualifiedFighters = [];
+let winnerFighters = [];
+let podio = [];
+
+
+
+copyFighters = fighters.map(f => ({ name: f.name, power: f.power }))
+copyWeapons = weapons.map(w => ({ name: w.name, power: w.power }))
 
 let numCasual = 0;
 const randomGenerate = (min, max) => {
@@ -8,94 +16,106 @@ const randomGenerate = (min, max) => {
     return numCasual;
 }
 
-//FASE 1. Assegno ad ogni combattente una proprietà weapon, dove salvare la sua arma casuale.
-copyFighters.forEach(f => {
-    f.weapon = "";
-    f.powerAfterTraining = f.power;
-})
-
-//Creo logica per assegnare ad ogni combattente un'arma casuale.
-console.log("=== FASE 1: scelta dell'arma ===")
-copyFighters.forEach(f => {
-    let index = randomGenerate(0, copyWeapons.length - 1);
-
-    let selectedWeapon = copyWeapons[index];
-    f.weapon = selectedWeapon.name;
-    copyWeapons.splice(index, 1)
-
-})
-//mostra in console l'output sotto forma di tabella
-console.table(copyFighters)
-
-
-console.log("=== FASE 2: Allenamento ===")
-
-copyFighters.forEach(f => {
-    let increment = randomGenerate(1, 100);
-    f.powerAfterTraining = f.power * increment;
-
-})
-
-console.table(copyFighters)
-
-
-
-
-console.log("=== FASE 3: Qualificazione ===")
-
-let qualifiedFighters = copyFighters.filter((f) => f.powerAfterTraining > 2000)
-
-console.table(qualifiedFighters)
-
-
-
-console.log("=== FASE 4: Combattimento ===")
-//creiamo il nuovo combattente che prenderà parte solo se sono dispari i qualificati.
-let newFighter = {name: "Robot", powerAfterTraining: 4000, weapon:{name: "Laser", power: 100}}
-
-if(qualifiedFighters.length % 2 !== 0){
-    qualifiedFighters.push(newFighter);
-}
-let winnerFighters = [];
-
-for(let i = 0; i < qualifiedFighters.length; i+=2){
-    if(qualifiedFighters[i].powerAfterTraining > qualifiedFighters[i + 1].powerAfterTraining){
-        console.log(`Vince il combattimento ${qualifiedFighters[i].name} contro ${qualifiedFighters[i + 1].name}`)
-        winnerFighters.push(qualifiedFighters[i])
-    } else if(qualifiedFighters[i].powerAfterTraining < qualifiedFighters[i + 1].powerAfterTraining){
-        console.log(`Vince il combattimento ${qualifiedFighters[i + 1].name} contro ${qualifiedFighters[i].name}`)
-         winnerFighters.push(qualifiedFighters[i + 1])
-    }else{
-        console.log(`Vince il combattimento ${qualifiedFighters[i].name} contro ${qualifiedFighters[i + 1].name} sfruttando il fattore casa.`)
-         winnerFighters.push(qualifiedFighters[i])
-    }
-    
-}
-console.log("-------------------------------")
-console.log("Tabella che mostra i vincitori di ogni incontro")
-console.table(winnerFighters)
-
-
-
-
-console.log("-------------------------------")
-console.log("=== FASE 5: Premiazione - Podio ===")
-
-winnerFighters.sort((a,b) => {
-    return b.powerAfterTraining - a.powerAfterTraining
-})
-//una volta ordinati in maniera decrescente con il metodo slice, accorcio l'array affinchè ci siano solo i primi tre elementi (i primi tre classificati).
-let podio = winnerFighters.slice(0, 3)
-console.table(podio)
-
-
-
 
 
 function startTournament() {
-  
+    resetTournament()
+    //FASE 1. Assegno ad ogni combattente una proprietà weapon, dove salvare la sua arma casuale.
+    copyFighters.forEach(f => {
+        f.weapon = "";
+        f.powerAfterTraining = f.power;
+    })
+
+    //Creo logica per assegnare ad ogni combattente un'arma casuale.
+    console.log("=== FASE 1: scelta dell'arma ===")
+    copyFighters.forEach(f => {
+        let index = randomGenerate(0, copyWeapons.length - 1);
+
+        let selectedWeapon = copyWeapons[index];
+        f.weapon = selectedWeapon.name;
+        copyWeapons.splice(index, 1)
+
+    })
+    //mostra in console l'output sotto forma di tabella
+    console.table(copyFighters)
+
+
+    console.log("=== FASE 2: Allenamento ===")
+
+    copyFighters.forEach(f => {
+        let increment = randomGenerate(1, 100);
+        f.powerAfterTraining = f.power * increment;
+
+    })
+
+    console.table(copyFighters)
+
+
+
+
+    console.log("=== FASE 3: Qualificazione ===")
+
+    qualifiedFighters = copyFighters.filter((f) => f.powerAfterTraining > 2000)
+
+    console.table(qualifiedFighters)
+
+
+
+    console.log("=== FASE 4: Combattimento ===")
+    //creiamo il nuovo combattente che prenderà parte solo se sono dispari i qualificati.
+    let newFighter = { name: "Robot", powerAfterTraining: 4000, weapon: { name: "Laser", power: 100 } }
+
+    if (qualifiedFighters.length % 2 !== 0) {
+        qualifiedFighters.push(newFighter);
+    }
+    
+
+    for (let i = 0; i < qualifiedFighters.length; i += 2) {
+        if (qualifiedFighters[i].powerAfterTraining > qualifiedFighters[i + 1].powerAfterTraining) {
+            console.log(`Vince il combattimento ${qualifiedFighters[i].name} contro ${qualifiedFighters[i + 1].name}`)
+            winnerFighters.push(qualifiedFighters[i])
+        } else if (qualifiedFighters[i].powerAfterTraining < qualifiedFighters[i + 1].powerAfterTraining) {
+            console.log(`Vince il combattimento ${qualifiedFighters[i + 1].name} contro ${qualifiedFighters[i].name}`)
+            winnerFighters.push(qualifiedFighters[i + 1])
+        } else {
+            console.log(`Vince il combattimento ${qualifiedFighters[i].name} contro ${qualifiedFighters[i + 1].name} sfruttando il fattore casa.`)
+            winnerFighters.push(qualifiedFighters[i])
+        }
+
+    }
+    console.log("-------------------------------")
+    console.log("Tabella che mostra i vincitori di ogni incontro")
+    console.table(winnerFighters)
+
+
+
+
+    console.log("-------------------------------")
+    console.log("=== FASE 5: Premiazione - Podio ===")
+
+    winnerFighters.sort((a, b) => {
+        return b.powerAfterTraining - a.powerAfterTraining
+    })
+    //una volta ordinati in maniera decrescente con il metodo slice, accorcio l'array affinchè ci siano solo i primi tre elementi (i primi tre classificati).
+    podio = winnerFighters.slice(0, 3)
+    console.table(podio)
 }
 
-function resetTournament() {
 
+function resetTournament() {
+    let fase1Section = document.getElementById("fase1")
+    let fase2Section = document.getElementById("fase2")
+    let fase3Section = document.getElementById("fase3")
+    let fase4Section = document.getElementById("fase4")
+    let fase5Section = document.getElementById("fase5")
+    copyFighters = fighters.map(f => ({ name: f.name, power: f.power }))
+    copyWeapons = weapons.map(w => ({ name: w.name, power: w.power }))
+    qualifiedFighters = [];
+    winnerFighters = [];
+    podio = [];
+    fase1Section.innerHTML = "";
+    fase2Section.innerHTML = "";
+    fase3Section.innerHTML = "";
+    fase4Section.innerHTML = "";
+    fase5Section.innerHTML = "";
 }
--- render.js
+++ render.js
@@ -1,218 +1,223 @@
-let fase1Section = document.getElementById("fase1")
+function renderTables(){
 
-let table1 = document.createElement("table")
-
-let firstRow = document.createElement("tr")
-
-let arrayFirstRow = ["Nome", "Potenza", "Arma"];
-
-arrayFirstRow.forEach((colName) => {
+    let fase1Section = document.getElementById("fase1")
+    
+    let table1 = document.createElement("table")
+    
+    let firstRow = document.createElement("tr")
+    
+    let arrayFirstRow = ["Nome", "Potenza", "Arma"];
+    
+    arrayFirstRow.forEach((colName) => {
+        let th = document.createElement("th")
+        th.textContent = colName;
+        firstRow.appendChild(th)
+    })
+    table1.appendChild(firstRow)
+    
+    copyFighters.forEach((f) => {
+        let row = document.createElement("tr");
+    
+        let tdName = document.createElement("td");
+        tdName.textContent = f.name;
+        row.appendChild(tdName);
+    
+         let tdPower = document.createElement("td");
+        tdPower.textContent = f.power;
+        row.appendChild(tdPower);
+    
+         let tdWeapon = document.createElement("td");
+        tdWeapon.textContent = f.weapon;
+        row.appendChild(tdWeapon);
+    
+    
+        table1.appendChild(row);
+    })
+    
+    fase1Section.appendChild(table1)
+    
+    
+    
+    
+    let fase2Section = document.getElementById("fase2")
+    
+    let table2 = document.createElement("table")
+    
+    let firstRow2 = document.createElement("tr")
+    let arrayFirstRow2 = ["Nome", "Potenza", "Arma", "Potenza post allenamento"];
+    
+    arrayFirstRow2.forEach((colName) => {
     let th = document.createElement("th")
-    th.textContent = colName;
-    firstRow.appendChild(th)
-})
-table1.appendChild(firstRow)
+    th.textContent = colName
+    firstRow2.appendChild(th)
+    })
+    table2.appendChild(firstRow2)
+    
+    
+    copyFighters.forEach((f) => {
+        let row = document.createElement("tr")
+    
+        let tdName = document.createElement("td")
+        tdName.textContent = f.name;
+        row.appendChild(tdName);
+    
+        let tdPower = document.createElement("td")
+        tdPower.textContent = f.power;
+        row.appendChild(tdPower);
+    
+        let tdWeapon = document.createElement("td")
+        tdWeapon.textContent = f.weapon;
+        row.appendChild(tdWeapon);
+    
+        let tdPowerAfterTraining = document.createElement("td")
+        tdPowerAfterTraining.textContent = f.powerAfterTraining;
+        row.appendChild(tdPowerAfterTraining);
+    
+        table2.appendChild(row)
+    })
+    
+    fase2Section.appendChild(table2)
+    
+    
+    
+    
+    let fase3Section = document.getElementById("fase3")
+    
+    let table3 = document.createElement("table")
+    
+    let firstRow3 = document.createElement("tr")
+    let arrayFirstRow3 = ["Nome", "Potenza", "Arma", "Potenza post allenamento"];
+    
+    arrayFirstRow3.forEach((colName) => {
+    let th = document.createElement("th")
+    th.textContent = colName
+    firstRow3.appendChild(th)
+    })
+    table3.appendChild(firstRow3)
+    
+    
+    qualifiedFighters.forEach((f) => {
+        let row = document.createElement("tr")
+    
+        let tdName = document.createElement("td")
+        tdName.textContent = f.name;
+        row.appendChild(tdName);
+    
+        let tdPower = document.createElement("td")
+        tdPower.textContent = f.power;
+        row.appendChild(tdPower);
+    
+        let tdWeapon = document.createElement("td")
+        tdWeapon.textContent = f.weapon;
+        row.appendChild(tdWeapon);
+    
+        let tdPowerAfterTraining = document.createElement("td")
+        tdPowerAfterTraining.textContent = f.powerAfterTraining;
+        row.appendChild(tdPowerAfterTraining);
+    
+        table3.appendChild(row)
+    })
+    
+    fase3Section.appendChild(table3)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    let fase4Section = document.getElementById("fase4")
+    
+    let table4 = document.createElement("table")
+    
+    let firstRow4 = document.createElement("tr")
+    let arrayFirstRow4 = ["Nome", "Potenza", "Arma", "Potenza post allenamento"];
+    
+    arrayFirstRow4.forEach((colName) => {
+    let th = document.createElement("th")
+    th.textContent = colName
+    firstRow4.appendChild(th)
+    })
+    table4.appendChild(firstRow4)
+    
+    
+    winnerFighters.forEach((f) => {
+        let row = document.createElement("tr")
+    
+        let tdName = document.createElement("td")
+        tdName.textContent = f.name;
+        row.appendChild(tdName);
+    
+        let tdPower = document.createElement("td")
+        tdPower.textContent = f.power;
+        row.appendChild(tdPower);
+    
+        let tdWeapon = document.createElement("td")
+        tdWeapon.textContent = f.weapon;
+        row.appendChild(tdWeapon);
+    
+        let tdPowerAfterTraining = document.createElement("td")
+        tdPowerAfterTraining.textContent = f.powerAfterTraining;
+        row.appendChild(tdPowerAfterTraining);
+    
+        table4.appendChild(row)
+    })
+    
+    fase4Section.appendChild(table4)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    let fase5Section = document.getElementById("fase5")
+    
+    let table5 = document.createElement("table")
+    
+    let firstRow5 = document.createElement("tr")
+    let arrayFirstRow5 = ["Nome", "Potenza", "Arma", "Potenza post allenamento"];
+    
+    arrayFirstRow5.forEach((colName) => {
+    let th = document.createElement("th")
+    th.textContent = colName
+    firstRow5  .appendChild(th)
+    })
+    table5.appendChild(firstRow5)
+    
+    
+    podio.forEach((f) => {
+        let row = document.createElement("tr")
+    
+        let tdName = document.createElement("td")
+        tdName.textContent = f.name;
+        row.appendChild(tdName);
+    
+        let tdPower = document.createElement("td")
+        tdPower.textContent = f.power;
+        row.appendChild(tdPower);
+    
+        let tdWeapon = document.createElement("td")
+        tdWeapon.textContent = f.weapon;
+        row.appendChild(tdWeapon);
+    
+        let tdPowerAfterTraining = document.createElement("td")
+        tdPowerAfterTraining.textContent = f.powerAfterTraining;
+        row.appendChild(tdPowerAfterTraining);
+    
+        table5.appendChild(row)
+    })
+    
+    fase5Section.appendChild(table5)
+}
 
-copyFighters.forEach((f) => {
-    let row = document.createElement("tr");
 
-    let tdName = document.createElement("td");
-    tdName.textContent = f.name;
-    row.appendChild(tdName);
-
-     let tdPower = document.createElement("td");
-    tdPower.textContent = f.power;
-    row.appendChild(tdPower);
-
-     let tdWeapon = document.createElement("td");
-    tdWeapon.textContent = f.weapon;
-    row.appendChild(tdWeapon);
-
-
-    table1.appendChild(row);
-})
-
-fase1Section.appendChild(table1)
-
-
-
-
-let fase2Section = document.getElementById("fase2")
-
-let table2 = document.createElement("table")
-
-let firstRow2 = document.createElement("tr")
-let arrayFirstRow2 = ["Nome", "Potenza", "Arma", "Potenza post allenamento"];
-
-arrayFirstRow2.forEach((colName) => {
-let th = document.createElement("th")
-th.textContent = colName
-firstRow2.appendChild(th)
-})
-table2.appendChild(firstRow2)
-
-
-copyFighters.forEach((f) => {
-    let row = document.createElement("tr")
-
-    let tdName = document.createElement("td")
-    tdName.textContent = f.name;
-    row.appendChild(tdName);
-
-    let tdPower = document.createElement("td")
-    tdPower.textContent = f.power;
-    row.appendChild(tdPower);
-
-    let tdWeapon = document.createElement("td")
-    tdWeapon.textContent = f.weapon;
-    row.appendChild(tdWeapon);
-
-    let tdPowerAfterTraining = document.createElement("td")
-    tdPowerAfterTraining.textContent = f.powerAfterTraining;
-    row.appendChild(tdPowerAfterTraining);
-
-    table2.appendChild(row)
-})
-
-fase2Section.appendChild(table2)
-
-
-
-
-let fase3Section = document.getElementById("fase3")
-
-let table3 = document.createElement("table")
-
-let firstRow3 = document.createElement("tr")
-let arrayFirstRow3 = ["Nome", "Potenza", "Arma", "Potenza post allenamento"];
-
-arrayFirstRow3.forEach((colName) => {
-let th = document.createElement("th")
-th.textContent = colName
-firstRow3.appendChild(th)
-})
-table3.appendChild(firstRow3)
-
-
-qualifiedFighters.forEach((f) => {
-    let row = document.createElement("tr")
-
-    let tdName = document.createElement("td")
-    tdName.textContent = f.name;
-    row.appendChild(tdName);
-
-    let tdPower = document.createElement("td")
-    tdPower.textContent = f.power;
-    row.appendChild(tdPower);
-
-    let tdWeapon = document.createElement("td")
-    tdWeapon.textContent = f.weapon;
-    row.appendChild(tdWeapon);
-
-    let tdPowerAfterTraining = document.createElement("td")
-    tdPowerAfterTraining.textContent = f.powerAfterTraining;
-    row.appendChild(tdPowerAfterTraining);
-
-    table3.appendChild(row)
-})
-
-fase3Section.appendChild(table3)
-
-
-
-
-
-
-
-
-
-let fase4Section = document.getElementById("fase4")
-
-let table4 = document.createElement("table")
-
-let firstRow4 = document.createElement("tr")
-let arrayFirstRow4 = ["Nome", "Potenza", "Arma", "Potenza post allenamento"];
-
-arrayFirstRow4.forEach((colName) => {
-let th = document.createElement("th")
-th.textContent = colName
-firstRow4.appendChild(th)
-})
-table4.appendChild(firstRow4)
-
-
-winnerFighters.forEach((f) => {
-    let row = document.createElement("tr")
-
-    let tdName = document.createElement("td")
-    tdName.textContent = f.name;
-    row.appendChild(tdName);
-
-    let tdPower = document.createElement("td")
-    tdPower.textContent = f.power;
-    row.appendChild(tdPower);
-
-    let tdWeapon = document.createElement("td")
-    tdWeapon.textContent = f.weapon;
-    row.appendChild(tdWeapon);
-
-    let tdPowerAfterTraining = document.createElement("td")
-    tdPowerAfterTraining.textContent = f.powerAfterTraining;
-    row.appendChild(tdPowerAfterTraining);
-
-    table4.appendChild(row)
-})
-
-fase4Section.appendChild(table4)
-
-
-
-
-
-
-
-
-
-
-
-let fase5Section = document.getElementById("fase5")
-
-let table5 = document.createElement("table")
-
-let firstRow5 = document.createElement("tr")
-let arrayFirstRow5 = ["Nome", "Potenza", "Arma", "Potenza post allenamento"];
-
-arrayFirstRow5.forEach((colName) => {
-let th = document.createElement("th")
-th.textContent = colName
-firstRow5  .appendChild(th)
-})
-table5.appendChild(firstRow5)
-
-
-podio.forEach((f) => {
-    let row = document.createElement("tr")
-
-    let tdName = document.createElement("td")
-    tdName.textContent = f.name;
-    row.appendChild(tdName);
-
-    let tdPower = document.createElement("td")
-    tdPower.textContent = f.power;
-    row.appendChild(tdPower);
-
-    let tdWeapon = document.createElement("td")
-    tdWeapon.textContent = f.weapon;
-    row.appendChild(tdWeapon);
-
-    let tdPowerAfterTraining = document.createElement("td")
-    tdPowerAfterTraining.textContent = f.powerAfterTraining;
-    row.appendChild(tdPowerAfterTraining);
-
-    table5.appendChild(row)
-})
-
-fase5Section.appendChild(table5)
 
 
 
@@ -221,6 +226,8 @@ let startBtn = document.getElementById("startBtn")
 let resetBtn = document.getElementById("resetBtn")
 
 
-startBtn.addEventListener("click", () =>{
-    
+startBtn.addEventListener("click", () => {
+    startTournament();
+    renderTables();
 })
+resetBtn.addEventListener("click", resetTournament);
